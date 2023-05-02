99784e5f7404c1f78f6dc5f3e45d96bb790b7171
nikitanovikov <Vertihost@yandex.com>
2023-05-02 16:03:31 +0300

    fun commit

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 # Created by nikitanovikov at 02.05.2023
 
-# Enter feature name here
-# Enter feature description here
-# Enter scenario name here
-# Enter steps here
+def hello():
+    return "Hello Lip"
+
+print(hello())
