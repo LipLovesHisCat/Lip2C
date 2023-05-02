e8e7c625e49a19d9750dd67354d8797bad98e1b4
nikitanovikov <Vertihost@yandex.com>
2023-05-02 23:11:00 +0300

    Add PyQt6

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
-# Created by nikitanovikov at 02.05.2023
+from PyQt6 import QtCore, QtGui, QtWidgets
 
 def hello():
     return "Мама! Добро пожаловать в мастер создания документации! ψ Сбросим с себя оковы, долой труд дешевый!"
-
 
 print(hello())
