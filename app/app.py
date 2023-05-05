aefc201a161d307642b4090f5507e239eba20b99
LipLovesHisCat <liparit220900@gmail.com>
2023-05-05 18:59:31 +0300

    о

--- app/app.py
+++ app/app.py
@@ -1,6 +1,24 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
+from window.WelcomeWindow import Ui_Dialog
 
-def hello():
-    return "Добро пожаловать в мастер создания документации! ψ Сбросим с себя оковы, долой труд дешевый!"
 
-print(hello())
+class WelcomeWindow(QtWidgets.QDialog, Ui_Dialog):
+    def __init__(self, parent=None):
+        super(WelcomeWindow, self).__init__()
+        self.setupUi(self)
+        self.pushButton_2.clicked.connect(self.program_exit)
+    def program_exit(self):
+        self.close()
+
+def start_program():
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = WelcomeWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+
+start_program()
